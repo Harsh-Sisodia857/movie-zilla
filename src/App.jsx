5aceea568f670d78a2275589040ef5e84cbb18ab
Harsh-Sisodia857 <harsh2012007@akgec.ac.in>
2024-01-12 18:03:51 +0530

    create a redux store to fetch movie

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './component/Navbar';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/store/slice/moviesSlice.js
@@ -0,0 +1,79 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+    header: "Trending",
+    totalPage: null,
+    movies: [],
+    searchedMovies: [],
+    trending: [],
+    upcoming: [],
+    page: 1,
+    activegenre: 28,
+    genres: [],
+    loader: true,
+    backgenre: false,
+    user: null,
+};
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const fetchMovies = createAsyncThunk('movies/fetchMovies', async ({ activegenre, page, query }) => {
+    let apiUrl = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_origin_country=IN&page=${page}`;
+
+    if (activegenre) {
+        apiUrl += `&with_genres=${activegenre}`;
+    }
+
+    if (query) {
+        apiUrl = `${BASE_URL}/search/movie?api_key=${API_KEY}&with_origin_country=IN&language=en-US&query=${query}&page=1&include_adult=false`;
+    }
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    return data;
+});
+
+const moviesSlice = createSlice({
+    name: 'movies',
+    initialState,
+    reducers: {
+        setHeader: (state, action) => {
+            state.header = action.payload;
+        },
+        setPage: (state, action) => {
+            state.page = action.payload;
+        },
+        setActiveGenre: (state, action) => {
+            state.activegenre = action.payload;
+        },
+        setGenres: (state, action) => {
+            state.genres = action.payload;
+        },
+        setBackGenre: (state, action) => {
+            state.backgenre = action.payload;
+        },
+        setLoader: (state, action) => {
+            state.loader = action.payload;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchMovies.fulfilled, (state, action) => {
+                state.movies = action.payload.results; 
+                state.totalPage = action.payload.total_pages;
+                state.loader = false;
+            })
+            .addCase(fetchMovies.pending, (state) => {
+                state.loader = true;
+            })
+            .addCase(fetchMovies.rejected, (state) => {
+                state.loader = false;
+            });
+    },
+
+});
+
+export const { setHeader, setPage, setActiveGenre, setGenres, setBackGenre, setLoader } = moviesSlice.actions;
+export default moviesSlice.reducer;
