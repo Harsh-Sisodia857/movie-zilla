27410c6de6467ebe325e17e41947c5dcaa136bab
Harsh-Sisodia857 <harsh2012007@akgec.ac.in>
2024-01-23 01:38:13 +0530

    Update User Slice

--- src/store/slice/userSlice.js
+++ src/store/slice/userSlice.js
@@ -1,13 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import {
     GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
     signInWithPopup,
     signOut,
 } from "firebase/auth";
 import { getFirestore, doc, setDoc, collection } from "firebase/firestore";
-import { auth, storageRef } from "../../../firebase";
+import { auth } from "../../../firebase";
 import { getDatabase, ref, set } from "firebase/database";
 
 const initialState = {
@@ -54,60 +52,6 @@ export const googleLogin = () => async (dispatch) => {
         dispatch(setUser(user));
     } catch (error) {
         console.error("Google login error:", error);
-    }
-};
-
-export const emailPasswordLogin = (email, password) => async (dispatch) => {
-    try {
-        const result = await signInWithEmailAndPassword(auth, email, password);
-        console.log(result);
-        const { uid, email, displayName, photoURL } = result.user;
-        const user = { uid, email, displayName, photoURL };
-
-        console.log(user);
-        dispatch(setUser(user));
-    } catch (error) {
-        console.error("Email/password login error:", error);
-    }
-};
-
-export const registerWithEmailPassword = (email, password, name, profilePicture) => async (dispatch) => {
-    try {
-        const result = await createUserWithEmailAndPassword(auth, email, password);
-        const { uid, email: userEmail } = result.user;
-        console.log("Name:", name);
-        console.log("Email:", userEmail);
-        console.log("Password:", password);
-        // Upload profile picture to Firebase Storage
-        const db = getDatabase();
-
-        set(ref(db, '/users'), {
-            profile_picture: profilePicture
-        })
-        const downloadURL = await profilePictureRef.getDownloadURL();
-
-        // Update user profile with the provided name and profile picture URL
-        await updateProfile(auth.currentUser, {
-            displayName: name,
-            photoURL: downloadURL,
-        });
-
-        const userData = {
-            uid,
-            userEmail,
-            displayName: name,
-            photoURL: downloadURL,
-        };
-
-        // Save additional user data to Firestore
-        const firestore = getFirestore();
-        const usersCollection = collection(firestore, "users");
-
-        await setDoc(doc(usersCollection, uid), userData);
-
-        dispatch(setUser(userData));
-    } catch (error) {
-        console.error("Registration error:", error);
     }
 };
 
